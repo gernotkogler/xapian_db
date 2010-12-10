b652b6ea0b77aa01c8776387f61960a07983d366
Gernot Kogler <gernot@kogler-informatik.ch>
2010-12-10 10:08:31 +0100

    Support for language configuration from the config file

--- lib/xapian_db/railtie.rb
+++ lib/xapian_db/railtie.rb
@@ -35,6 +35,7 @@ module XapianDb
         end
         config.adapter adapter.to_sym
         config.writer writer.to_sym
+        config.language(env_config["language"]) if env_config["language"]
       end
 
     end
