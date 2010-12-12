f2da27e85a72a73edf780fe66ce6a654495c83df
Gernot Kogler <gernot@kogler-informatik.ch>
2010-12-12 08:55:50 +0100

    Added the stopper repository

---
+++ lib/xapian_db/repositories/stopper.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+module XapianDb
+  module Repositories
+
+    # The stopper is a repository that manages stoppers for the supported
+    # languges
+    # @author Gernot Kogler
+    class Stopper
+
+      class << self
+
+        # Get or build the stopper for a language
+        # @param [String] iso_cd The iso code for the language (:en, :de ...)
+        # @return [Xapian::SimpleStopper] The Stopper for this lanugage
+        def stopper_for(iso_cd)
+          @stoppers ||= {}
+          return nil if iso_cd.nil?
+          key = iso_cd.to_sym
+
+          # Do we already have a stopper for this language?
+          return @stoppers[key] unless @stoppers[key].nil?
+
+          # Do we support this language?
+          unless (LANGUAGE_MAP.keys + [:none]).include?(key)
+            raise ArgumentError.new "Language #{iso_cd} is not supported by XapianDb (remember to use the language iso codes)"
+          end
+
+          # Let's build the stopper
+          stopper = Xapian::SimpleStopper.new
+          stopwords_file = File.join(File.dirname(__FILE__), '../stopwords', "#{iso_cd}.txt")
+          open(stopwords_file, "r") do |file|
+            while word = file.gets
+              stopper.add word
+            end
+          end
+          @stoppers[key] = stopper
+        end
+
+      end
+
+    end
+
+  end
+end
---
+++ spec/xapian_db/repositories/stopper_spec.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe XapianDb::Repositories::Stopper do
+
+  describe ".stopper_for(iso_cd)" do
+    it "returns nil for iso code nil" do
+      XapianDb::Repositories::Stopper.stopper_for(nil).should_not be
+    end
+
+    it "returns a Xapian::SimpleStopper object for a supported language iso code" do
+      XapianDb::Repositories::Stopper.stopper_for(:de).should be_a_kind_of Xapian::SimpleStopper
+    end
+
+    it "caches already used stoppers" do
+      stopper = XapianDb::Repositories::Stopper.stopper_for(:de)
+      XapianDb::Repositories::Stopper.stopper_for(:de).should be_equal stopper
+    end
+
+    it "raises an argument error if the language is not supported" do
+      lambda {XapianDb::Repositories::Stopper.stopper_for(:not_supported)}.should raise_error ArgumentError
+    end
+
+  end
+
+end
