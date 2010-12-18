7ea2af5aec3ccc1e99bd73851aef39784a343c44
Gernot Kogler <gernot@kogler-informatik.ch>
2010-12-18 10:25:18 +0100

    No features so far planned

--- features/step_definitions/common_steps.rb
+++
@@ -1,7 +0,0 @@
-Given /^no file exists at "([^\"]*)"$/ do |path|
-  FileUtils.rm_rf(File.dirname(__FILE__) + "/../../#{path}") if File.exist? path
-end
-
-Then /^I should find a directory at "([^\"]*)"$/ do |path|
-  File.exist?(File.dirname(__FILE__) + "/../../#{path}").should be_true
-end
--- features/support/env.rb
+++
@@ -1,3 +0,0 @@
-require 'cucumber'
-require 'spec'
-require 'fileutils'
