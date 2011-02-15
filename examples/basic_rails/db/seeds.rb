4d5caae5c17fae22a22638921ed1c5e151ef45ce
Gernot Kogler <gernot@kogler-informatik.ch>
2011-02-15 07:50:19 +0100

    Reduced the number of test data in the example app

--- examples/basic_rails/db/seeds.rb
+++ examples/basic_rails/db/seeds.rb
@@ -8,21 +8,21 @@ Person.connection.execute("delete from sqlite_sequence where name='people'")
 Address.connection.execute("delete from addresses")
 Address.connection.execute("delete from sqlite_sequence where name='addresses'")
 
-# 2: create 1000 addresses
-1000.times do
+# 2: create 10 addresses
+10.times do
   Address.create :street => Forgery::Address.street_address,
                  :zip    => Forgery::Address.zip,
                  :city   => Forgery::Address.city
 end
 
-# 3: create 10'000 people
+# 3: create 100 people
 languages = %w(da nl en fi fr de hu it nb nn no pt ro ru es sv tr)
-1000.times do
+100.times do
   Person.create :name          => Forgery::Name.last_name,
                 :first_name    => Forgery::Name.first_name,
                 :date_of_birth => Forgery::Date.date(:future => false, :min_delta => 7200, :max_delta => 30000),
                 :language      => languages[Forgery::Basic.number(:at_least => 0, :at_most => languages.size - 1)],
-                :address_id    => Forgery::Basic.number(:at_least => 1, :at_most => 1000),
+                :address_id    => Forgery::Basic.number(:at_least => 1, :at_most => 10),
                 :notes         => Forgery::LoremIpsum.words(Forgery::Basic.number(:at_least => 1, :at_most => 50))
 end
 
