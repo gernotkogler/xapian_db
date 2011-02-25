ea7464aa58f803e01529aa3d3227695052ff9a8b
Gernot Kogler <gernot@kogler-informatik.ch>
2011-02-25 07:09:07 +0100

    fix to make the :memory: database option working again

--- lib/xapian_db/railtie.rb
+++ lib/xapian_db/railtie.rb
@@ -21,13 +21,13 @@ module XapianDb
       if File.exist?(config_file_path)
         db_config = YAML::load_file config_file_path
         env_config = db_config[Rails.env]
-        database_path    = File.expand_path(env_config["database"]) || ":memory:"
+        database_path    = env_config["database"] || ":memory:"
         adapter          = env_config["adapter"]  || :active_record
         writer           = env_config["writer"]   || :direct
         beanstalk_daemon = env_config["beanstalk_daemon"]
       else
         # No config file, set the defaults
-        Rails.env == "test" ? database_path = ":memory:" : database_path = File.expand_path("db/xapian_db/#{Rails.env}")
+        Rails.env == "test" ? database_path = ":memory:" : database_path = "db/xapian_db/#{Rails.env}"
         adapter          = :active_record
         writer           = :direct
         beanstalk_daemon = nil
@@ -38,7 +38,7 @@ module XapianDb
         if database_path == ":memory:"
           config.database :memory
         else
-          config.database database_path
+          config.database File.expand_path database_path
         end
         config.adapter adapter.to_sym
         config.writer writer.to_sym
