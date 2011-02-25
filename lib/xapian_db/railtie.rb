dfa00d8153c84610db6bb0fe9ffb8dc51cf950e6
Gernot Kogler <gernot@kogler-informatik.ch>
2011-02-25 09:12:27 +0100

    support for a missing configuration for the environment in xapian_db.yml (will use defaults if no config is given)

--- lib/xapian_db/railtie.rb
+++ lib/xapian_db/railtie.rb
@@ -21,29 +21,23 @@ module XapianDb
       if File.exist?(config_file_path)
         db_config = YAML::load_file config_file_path
         env_config = db_config[Rails.env]
-        database_path    = env_config["database"] || ":memory:"
-        adapter          = env_config["adapter"]  || :active_record
-        writer           = env_config["writer"]   || :direct
-        beanstalk_daemon = env_config["beanstalk_daemon"]
+        env_config ? configure_from(env_config) : configure_defaults
       else
         # No config file, set the defaults
-        Rails.env == "test" ? database_path = ":memory:" : database_path = "db/xapian_db/#{Rails.env}"
-        adapter          = :active_record
-        writer           = :direct
-        beanstalk_daemon = nil
+        configure_defaults
       end
 
       # Do the configuration
       XapianDb::Config.setup do |config|
-        if database_path == ":memory:"
+        if @database_path == ":memory:"
           config.database :memory
         else
-          config.database File.expand_path database_path
+          config.database File.expand_path @database_path
         end
-        config.adapter adapter.to_sym
-        config.writer writer.to_sym
-        config.beanstalk_daemon_url beanstalk_daemon
-        config.language(env_config["language"]) if env_config && env_config["language"]
+        config.adapter @adapter.to_sym
+        config.writer @writer.to_sym
+        config.beanstalk_daemon_url @beanstalk_daemon
+        config.language @language
       end
 
     end
@@ -54,6 +48,26 @@ module XapianDb
       blueprints_file_path = "#{Rails.root}/config/xapian_blueprints.rb"
       load blueprints_file_path if File.exist?(blueprints_file_path)
     end
+
+  private
+
+  # use the config options from the config file
+  def self.configure_from(env_config)
+    @database_path    = env_config["database"] || ":memory:"
+    @adapter          = env_config["adapter"]  || :active_record
+    @writer           = env_config["writer"]   || :direct
+    @beanstalk_daemon = env_config["beanstalk_daemon"]
+    @language         = env_config["language"]
+  end
+
+  # set default config options
+  def self.configure_defaults
+    Rails.env == "test" ? @database_path = ":memory:" : @database_path = "db/xapian_db/#{Rails.env}"
+    @adapter          = :active_record
+    @writer           = :direct
+    @beanstalk_daemon = nil
+    @language         = :en
+  end
 
   end
 end
