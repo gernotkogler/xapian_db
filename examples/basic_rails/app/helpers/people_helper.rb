4a4e0d783b27083d5110a1a1c6875bd639c4807b
Gernot Kogler <gernot@kogler-informatik.ch>
2011-02-26 08:23:26 +0100

    added features from the newest version to the rails sample app

--- examples/basic_rails/app/helpers/people_helper.rb
+++ examples/basic_rails/app/helpers/people_helper.rb
@@ -1,7 +1,7 @@
 module PeopleHelper
 
   def person_doc_as_string(doc)
-    "#{doc.name} #{doc.first_name}, #{doc.address['street']}, #{doc.address['zip']} #{doc.address['city']}"
+    "#{doc.name} #{doc.first_name}, #{doc.address['street']}, #{doc.address['zip']} #{doc.address['city']} (#{doc.score}%)"
   end
 
 end
