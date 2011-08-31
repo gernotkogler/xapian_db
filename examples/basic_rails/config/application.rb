774e39ad616675bcc80925c31484e5822d110913
Gernot Kogler <gernot.kogler@garaio.com>
2011-08-31 12:13:40 +0200

    upgraded the rails example to 3.1

--- examples/basic_rails/config/application.rb
+++ examples/basic_rails/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module BasicRails
   class Application < Rails::Application
+
+    config.assets.enabled = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
