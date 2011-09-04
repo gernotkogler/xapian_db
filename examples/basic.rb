7a8e70f68e3a6429b88348503849049babd4cafa
Gernot Kogler <gernot.kogler@garaio.com>
2011-09-04 19:39:10 +0200

    test the new type support in the basic ruby example

--- examples/basic.rb
+++ examples/basic.rb
@@ -15,10 +15,10 @@ db = XapianDb.create_db
 # could be an ActiveRecord or Datamapper Domain class
 class Person
 
-  attr_accessor :id, :name, :first_name
+  attr_accessor :id, :name, :first_name, :date_of_birth
 
   def initialize(data)
-    @id, @name, @first_name = data[:id], data[:name], data[:first_name]
+    @id, @name, @first_name, @date_of_birth = data[:id], data[:name], data[:first_name], data[:date_of_birth]
   end
 
 end
@@ -35,12 +35,13 @@ end
 XapianDb::DocumentBlueprint.setup(Person) do |blueprint|
   blueprint.attribute :name
   blueprint.attribute :first_name
+  blueprint.attribute :date_of_birth, :as => :date
 end
 
 # 5: Let's create some objects
-person_1 = Person.new(:id => 1, :name => "Kogler", :first_name => "Gernot")
-person_2 = Person.new(:id => 2, :name => "Frey",   :first_name => "Daniel")
-person_3 = Person.new(:id => 3, :name => "Garaio", :first_name => "Thomas")
+person_1 = Person.new :id => 1, :name => "Doe",    :first_name => "John",  :date_of_birth => Date.new(1970, 2, 16)
+person_2 = Person.new :id => 2, :name => "Smith",  :first_name => "Frank", :date_of_birth => Date.new(1966, 5, 2)
+person_3 = Person.new :id => 3, :name => "Willis", :first_name => "Frank", :date_of_birth => Date.new(1968, 11, 4)
 
 # 6: Now add them to the database
 blueprint = XapianDb::DocumentBlueprint.blueprint_for(Person)
@@ -49,17 +50,18 @@ db.store_doc(indexer.build_document_for(person_1))
 db.store_doc(indexer.build_document_for(person_2))
 db.store_doc(indexer.build_document_for(person_3))
 
-# 7: Now find the gem author ;-)
-puts "Searching for Gernot..."
-results = db.search("Gernot")
+# 7: Now find a person
+puts "Searching for John..."
+results = db.search("John")
 puts "We found #{results.size} documents"
 puts "And the first document looks like this:"
 doc  = results.first
 puts "name: #{doc.name}"
 puts "first name: #{doc.first_name}"
+puts "date of birth: #{doc.date_of_birth.strftime("%d.%m.%Y")}"
 
 # Get all facets (classes) for a search expression
-facets = db.facets("Gernot")
+facets = db.facets("Frank")
 puts "facets:"
 facets.each do |klass, count|
   puts "#{klass.name}: #{count} hits"
